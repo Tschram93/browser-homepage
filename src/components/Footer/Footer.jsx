24079c00f8441c319795cb351eaf0651044a8ad3
TS93 <tschram93@yahoo.com>
2022-06-21 16:02:45 -0400

    created footerlink component within the footer folder; imported react-icons

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,6 +1,6 @@
 const Footer = () => {
     return (
-        <footer>
+        <footer className="footer">
             Footer
         </footer>
     )
---
+++ src/components/Footer/FooterLinks/FooterLinks.jsx
@@ -0,0 +1,14 @@
+import * as FaIcons from 'react-icons/fa';
+
+
+
+const FooterLinks = () => {
+{/* this is the proper syntax for icons <FaIcons.FaBars /> */}
+    return (
+        <div>
+            FooterLinks
+        </div>
+    )
+}
+
+export default FooterLinks
