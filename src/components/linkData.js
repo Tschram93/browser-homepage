c1e49e0897ddb7ea561206ddfb302be4c762ffff
TS93 <tschram93@yahoo.com>
2022-06-23 14:32:38 -0400

    all linkData object information added in as objects within array

--- src/components/linkData.js
+++ src/components/linkData.js
@@ -1,6 +1,152 @@
 export const linkData = [
-    {
-        link: '',
-        websiteName: ''
+	{
+		categoryName: 'Animation',
+		link: 'https://icons8.com/animated-icons',
+		websiteName: 'Animated Icons',
+	},
+	{
+		categoryName: 'Animation',
+		link: 'https://flaticon.com/',
+		websiteName: 'FlatIcon',
+	},
+	{   
+        categoryName: 'Design Structure',
+		link: 'https://www.awwwards.com/',
+		websiteName: 'Awwwards',
+	},
+	{   
+        categoryName: 'Design Structure',
+		link: 'https://dribbble.com/',
+		websiteName: 'Dribble',
+	},
+	{   
+        categoryName: 'Design Structure',
+		link: 'https://color.hailpixel.com/',
+		websiteName: 'Hail Pixel',
+	},
+	{   
+        categoryName: 'Design Structure',
+		link: 'http://stylifyme.com/',
+		websiteName: 'StylifyMe',
+	},
+	{   
+        categoryName: 'Exercises',
+		link: 'https://exercism.io/',
+		websiteName: 'Exercism',
+	},
+	{   
+        categoryName: 'Exercises',
+		link: 'https://www.codewars.com/',
+		websiteName: 'CodeWars',
+	},
+	{   
+        categoryName: 'Fonts',
+		link: 'https://fontawesome.com/',
+		websiteName: 'Font Awesome',
+	},
+	{   
+        categoryName: 'Fonts',
+		link: 'https://fonts.google.com/',
+		websiteName: 'Google Fonts',
+	},
+	{   
+        categoryName: 'Freelance',
+		link: 'https://www.fiverr.com/',
+		websiteName: 'Fiverr',
+	},
+	{   
+        categoryName: 'Freelance',
+		link: 'https://www.freelancer.com/',
+		websiteName: 'FreeLancer',
+	},
+	{   
+        categoryName: 'Freelance',
+		link: 'https://www.upwork.com/',
+		websiteName: 'UpWork',
+	},
+	{   
+        categoryName: 'General',
+		link: 'https://caniuse.com/',
+		websiteName: 'CanIUse',
+	},
+	{   
+        categoryName: 'General',
+		link: 'https://css-tricks.com/',
+		websiteName: 'CSS-Tricks',
+	},
+	{   
+        categoryName: 'General',
+		link: 'https://www.emailjs.com/',
+		websiteName: 'EmailJS',
+	},
+	{   
+        categoryName: 'General',
+		link: 'https://imagecompressor.com/',
+		websiteName: 'Image Compressor',
+	},
+	{   
+        categoryName: 'General',
+		link: 'https://tinypng.com/',
+		websiteName: 'TinyPNG',
+	},
+	{   
+        categoryName: 'Hosting & Deployment',
+		link: 'https://www.heroku.com/',
+		websiteName: 'Heroku',
+	},
+	{   
+        categoryName: 'Hosting & Deployment',
+		link: 'https://www.netlify.com/',
+		websiteName: 'Netlify',
+	},
+	{   
+        categoryName: 'Stock Image Sources',
+		link: 'https://www.pexels.com/',
+		websiteName: 'Pexels',
+	},
+	{   
+        categoryName: 'Stock Image Sources',
+		link: 'https://undraw.co/illustrations',
+		websiteName: 'Undraw - Illusions',
+	},
+	{   
+        categoryName: 'Stock Image Sources',
+		link: 'https://unsplash.com/',
+		websiteName: 'Unsplash',
+	},
+	{   
+        categoryName: 'Learning',
+		link: 'https://www.freecodecamp.org/',
+		websiteName: 'FreeCodeCamp',
+	},
+	{   
+        categoryName: 'Learning',
+		link: 'https://mem.dev/',
+		websiteName: 'Mem-dev',
+	},
+	{   
+        categoryName: 'Learning',
+		link: 'https://www.theodinproject.com/',
+		websiteName: 'The Odin Project',
+	},
+	{   
+        categoryName: 'Logo',
+		link: 'https://www.logaster.com/',
+		websiteName: 'Logastar',
+	},
+	{   
+        categoryName: 'Mockup & WireFrame',
+		link: 'https://www.figma.com/',
+		websiteName: 'Figma',
+	},
+	{   
+        categoryName: 'Mockup & WireFrame',
+		link: 'https://zeplin.io/',
+		websiteName: 'Zeplin',
+	},
+	{   
+        categoryName: 'Stock Video Sources',
+		link: 'https://coverr.co/',
+		websiteName: 'Coverr',
     }
-]
+];
