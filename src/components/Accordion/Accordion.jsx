2dfe369ac17cca8073d62e5a11fd29f3247396e3
TS93 <tschram93@yahoo.com>
2022-06-23 14:38:46 -0400

    created accordion component folder and file for resource links in the footer

---
+++ src/components/Accordion/Accordion.jsx
@@ -0,0 +1,13 @@
+import { IconContext } from 'react-icons/lib';
+import { linkData } from '../linkData';
+import * as FaIcons from 'react-icons/fa';
+import './Accordion.css';
+
+
+const Accordion = () => {
+    return (
+    <div>Accordion</div>
+    )
+};
+
+export default Accordion;
